ac8c3ddb59bc051862c4ce701548c083150ff9e5
Riya Verma <77240974+Riya59@users.noreply.github.com>
2023-04-08 16:06:58 +0530

    Create FrogJump.java

---
+++ FrogJump.java
@@ -0,0 +1,26 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static int jump(int ind, int heights[], int dp[]){
+        if(ind == 0){
+            return 0;
+        }
+        if(dp[ind] != -1){
+            return dp[ind];
+        }
+        int left = jump(ind-1,heights,dp)+ Math.abs(heights[ind] - heights[ind-1]);
+        int right = Integer.MAX_VALUE;
+        if(ind > 1){
+             right = jump(ind-2,heights,dp) + Math.abs(heights[ind] - heights[ind - 2]);
+        }
+        return dp[ind] = Math.min(left,right);
+    }
+    public static int frogJump(int n, int heights[]) {
+
+        // Write your code here..
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return jump(n-1,heights,dp);
+    }
+
+}
