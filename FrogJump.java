8b44dacc08baeeb815c3babedab92d12a71f52f7
Riya Verma <77240974+Riya59@users.noreply.github.com>
2023-04-08 16:21:06 +0530

    Update FrogJump.java

--- FrogJump.java
+++ FrogJump.java
@@ -21,6 +21,21 @@ public class Solution {
         int dp[] = new int[n+1];
         Arrays.fill(dp,-1);
         return jump(n-1,heights,dp);
+        
+        //Space optimisation
+        /*
+        int prev = 0;
+        int prev1 = 0;
+        for(int i=1;i<n;i++){
+        int left = prev + Math.abs(heights[i] - heights[i-1]);
+        int right = Integer.MAX_VALUE;
+        if(i > 1){
+        right = prev1 + Math.abs(heights[i] - heights[i-1]);
+        }
+        int curr = min(left,right);
+        prev1 = prev;
+        prev = curr;
     }
+    return prev;
 
 }
